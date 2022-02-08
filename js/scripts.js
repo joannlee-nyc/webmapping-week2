f36f20d8f249f502190fc1704cd7668a1034da62
joannlee5 <jl4884@nyu.edu>
2022-02-08 17:18:42 -0500

    added code to append html

--- js/scripts.js
+++ js/scripts.js
@@ -25,26 +25,57 @@ var data = [
       '#F14E2D'
     ]
   },
-  {
-    id: 'kilauea2',
-    image: 'https://www.travelweekly.com/uploadedImages/Art/2021/1011/T1011KILAUEA_C_HR.jpg?n=9562&width=1540&height=866&mode=crop&Anchor=MiddleCenter',
-    palette: [
-      '#483F78',
-      '#F9EBBB',
-      '#DB4B3A',
-      '#8CAFD5',
-      '#FDE0DF'
-    ]
-  },
-  {
-    id: 'kilauea3',
-    image: 'https://pbs.twimg.com/media/Euii2H3UUAQ6APt?format=jpg&name=large',
-    palette: [
-      '#FDAE65',
-      '#2C3056',
-      '#B0BDD3',
-      '#BA3119',
-      '#1C2628'
-    ]
-  }
+  //{
+  //  id: 'kilauea2',
+  //  image: 'https://www.travelweekly.com/uploadedImages/Art/2021/1011/T1011KILAUEA_C_HR.jpg?n=9562&width=1540&height=866&mode=crop&Anchor=MiddleCenter',
+  //  palette: [
+  //    '#483F78',
+  //    '#F9EBBB',
+  //    '#DB4B3A',
+  //    '#8CAFD5',
+  //    '#FDE0DF'
+  //  ]
+//  },
+//  {
+//    id: 'kilauea3',
+//    image: 'https://pbs.twimg.com/media/Euii2H3UUAQ6APt?format=jpg&name=large',
+//    palette: [
+  //    '#FDAE65',
+  //    '#2C3056',
+  //    '#B0BDD3',
+  //    '#BA3119',
+  //    '#1C2628'
+//    ]
+//  }
 ]
+
+data.forEach(function(record) {
+  $('#container').append(`
+    <div id = "${record.id}" class='carousel'>
+      <div id = "photo">
+      <img class= "volcanoes" src="${record.image}">
+      </div>
+      <div id = "colors">
+        <div class = "swatch" id="swatch1">${record.palette[0]}
+        </div>
+        <div class = "swatch" id="swatch2">${record.palette[1]}
+        </div>
+        <div class = "swatch" id="swatch3">${record.palette[2]}
+        </div>
+        <div class = "swatch" id="swatch4">${record.palette[3]}
+        </div>
+        <div class = "swatch" id="swatch5">${record.palette[4]}
+        </div>
+    </div>
+  `)
+
+})
+
+
+  $("swatch1").css("background-color",palette[0]);
+  $("swatch2").css("background-color",palette[1]);
+  $("swatch3").css("background-color",palette[2]);
+  $("swatch4").css("background-color",palette[3]);
+  $("swatch5").css("background-color",palette[4]);
+
+})
